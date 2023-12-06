7deb4f14ee4d02074cdcfa3ee8d7eb8feb4c9a07
Dante Niewenhuis <d.niewenhuis@hotmail.com>
2023-12-06 20:38:18 +0100

    changes

--- opendc-experiments/opendc-experiments-surfsara/src/main/kotlin/org/opendc/experiments/surfsara/topology/TopologyFactories.kt
+++ opendc-experiments/opendc-experiments-surfsara/src/main/kotlin/org/opendc/experiments/surfsara/topology/TopologyFactories.kt
@@ -88,7 +88,7 @@ private fun ClusterSpec.toHostSpecs(random: RandomGenerator, powerModel: CpuPowe
     val unknownProcessingNode = ProcessingNode("unknown", "unknown", "unknown", coreCount)
     val unknownMemoryUnit = MemoryUnit("unknown", "unknown", -1.0, memoryPerHost)
     val machineModel = MachineModel(
-        List(cpuCount) { coreId -> ProcessingUnit(unknownProcessingNode, coreId, actualCpuSpeed) },
+        List(cpuCount) { cpuId -> ProcessingUnit(unknownProcessingNode, cpuId, actualCpuSpeed) },
         listOf(unknownMemoryUnit)
     )
 
